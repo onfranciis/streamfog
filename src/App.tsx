5e570dd7da040cd6e1f82ce5d29e127258303e8c
onfranciis <onukwuf@gmail.com>
2023-12-14 00:54:44 +0100

    feat: setup webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import "./App.css";
+import { useEffect, useRef, useState } from "react";
+import "./App.scss";
 
 function App() {
-  return <div></div>;
+  const VideoRef = useRef<HTMLVideoElement>(null);
+  const [MediaRecorders, setMediaRecorders] = useState<null | MediaStream>(
+    null
+  );
+
+  useEffect(() => {
+    const startCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: false,
+        });
+
+        setMediaRecorders(stream);
+      } catch (err) {
+        //
+      }
+    };
+
+    startCamera();
+  }, []);
+
+  useEffect(() => {
+    if (MediaRecorders && VideoRef.current) {
+      VideoRef.current.srcObject = MediaRecorders;
+    }
+  }, [MediaRecorders]);
+
+  return (
+    <div className="App">
+      <video ref={VideoRef} autoPlay></video>
+    </div>
+  );
 }
 
 export default App;

c75fa68c5f6e77935d04bfabadebbb3852c1ef84
onfranciis <onukwuf@gmail.com>
2023-12-18 02:04:09 +0100

    feat: create colors

---
+++ src/utils/color.ts
@@ -0,0 +1,9 @@
+export const colors = [
+  { name: "Red", rgb: [255, 0, 0] },
+  { name: "Green", rgb: [0, 255, 0] },
+  { name: "Blue", rgb: [0, 0, 255] },
+  { name: "Yellow", rgb: [255, 255, 0] },
+  { name: "Magenta", rgb: [255, 0, 255] },
+  { name: "Cyan", rgb: [0, 255, 255] },
+  { name: "Default", rgb: [255, 255, 255, 0] },
+];

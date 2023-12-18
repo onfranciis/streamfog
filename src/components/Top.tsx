c9181c01279a4f017e01090c14ad0de97dd14730
onfranciis <onukwuf@gmail.com>
2023-12-18 02:04:18 +0100

    feat: create Top

---
+++ src/components/Top.tsx
@@ -0,0 +1,11 @@
+const Top = () => {
+  return (
+    <div className="Top">
+      <a href="">
+        <img src="/streamfog.svg" alt="Streamfog" height={50} width={100} />
+      </a>
+    </div>
+  );
+};
+
+export default Top;

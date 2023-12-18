12b51fb27ca0e0d9d4881e614bac8525cc6ee007
onfranciis <onukwuf@gmail.com>
2023-12-18 02:08:53 +0100

    feat: add Logic to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { useEffect, useRef, useState } from "react";
 import "./App.scss";
+import "@tensorflow/tfjs";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useLogic } from "./utils/logic";
+import * as bodyPix from "@tensorflow-models/body-pix";
+import { IColors } from "./types";
+import ColorPalette from "./components/ColorPalette";
+import CanvasParent from "./components/CanvasParent";
+import Top from "./components/Top";
 
 function App() {
+  const [color, setColor] = useState([255, 255, 255, 0]);
+  const [imageSrc, setImageSrc] = useState<null | string>(null);
+  const { detectBodySegments } = useLogic();
   const VideoRef = useRef<HTMLVideoElement>(null);
+  const CanvasRef = useRef<HTMLCanvasElement>(null);
   const [MediaRecorders, setMediaRecorders] = useState<null | MediaStream>(
     null
   );
@@ -17,7 +28,7 @@ function App() {
 
         setMediaRecorders(stream);
       } catch (err) {
-        //
+        console.log(err);
       }
     };
 
@@ -25,14 +36,48 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (VideoRef.current) {
+    if (VideoRef.current && CanvasRef.current && MediaRecorders) {
       VideoRef.current.srcObject = MediaRecorders;
+
+      bodyPix.load().then((network) => {
+        detectBodySegments(network, CanvasRef, VideoRef, MediaRecorders);
+      });
     }
   }, [MediaRecorders]);
 
+  const handleClick = (color: IColors) => {
+    setColor(() => color.rgb);
+    setImageSrc(null);
+  };
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    try {
+      const file = e?.target?.files;
+      if (file) {
+        const src = URL.createObjectURL(file[0]);
+        setImageSrc(src);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <video ref={VideoRef} autoPlay></video>
+      <Top />
+
+      <CanvasParent color={color} imageSrc={imageSrc}>
+        <canvas ref={CanvasRef}></canvas>
+      </CanvasParent>
+
+      <video ref={VideoRef} autoPlay width="640" height="500"></video>
+
+      <div className="inputParent">
+        <p>Add your image</p>
+        <input type="file" onChange={handleInput} accept="image/*" />
+      </div>
+
+      <ColorPalette handleClick={handleClick} />
     </div>
   );
 }
